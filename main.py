f91b78a4d049852f2db951ee41f55e0dcd1d2ceb
ankitzin <ankit.zin279@gmail.com>
2022-10-15 19:32:24 +0530

    Path parameters Done

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def home():
+@app.get("/hello")
+def index():
     return "hello world"
+
+
+@app.get("/blog/all")
+def get_all_blogs():
+    return {"message":"this is all message"}
+
+
+@app.get("/blog/{blog_id}")
+def get_blog(blog_id: int):
+    return f"The id is {blog_id}"
+
+
