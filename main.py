e7207cd430b8ff9ca7c06dc8a3e6157564a5cc81
ankitzin <ankit.zin279@gmail.com>
2022-10-15 21:48:17 +0530

    Tags, Description, Summary done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 
 app = FastAPI()
@@ -14,13 +14,23 @@ def index():
 # def get_all_blogs():
 #     return {"message":"this is all message"}
 
-@app.get("/blog/all")
+@app.get("/blog/all",
+         tags=['blog'],
+         summary="Get all blogs",
+         description="Getting all Blogs with this Request.")
 def get_all_blogs(page=3, page_size: Optional[int] = None):
     return {"message": f"all {page_size} on page {page}."}
 
 
-@app.get('/blog/{blog_id}/comments/{comment_id}')
+@app.get('/blog/{blog_id}/comments/{comment_id}', tags=['blog','comment'])
 def get_comment(blog_id:int, comment_id:int, valid: bool = True, username: Optional[str] = None):
+    """
+    - **blog_id**: Mandatory Parameter
+    - **comment_id**: Mandatory Parameter
+    - **valid**: Optional Parameter
+    - **username**: Optional Parameter <br><br>
+    ```return: the info about the comment on Blog with respect to blog_id.```
+    """
     return {
         "message": f"{blog_id} for username {username}, {valid} comment."
     }
@@ -32,14 +42,18 @@ class BlogType(str, Enum):
     howto = 'howto'
 
 
-@app.get("/blog/type/{blog_type}")
+@app.get("/blog/type/{blog_type}", tags=['blog'])
 def get_blog_type(blog_type: BlogType):
     return {
         "message": f"Blog type is {blog_type}"
     }
 
 
-@app.get("/blog/{blog_id}")
-def get_blog(blog_id: int):
+@app.get("/blog/{blog_id}", status_code=status.HTTP_200_OK, tags=['blog'])
+def get_blog(blog_id: int, response: Response):
+    if blog_id > 5:
+        response.status_code= status.HTTP_404_NOT_FOUND
+        return {"message": f"this {blog_id} not found."}
+
     return {"message": f"this is {blog_id}"}
 
--- readme.md
+++ readme.md
@@ -6,3 +6,14 @@
  predefined Parameters
  Query parameters
 ```
+
+#### Second Section
+
+```commandline
+Status codes
+tags
+summary and description
+```
+
+- hello <br>
+world
